bb8f8a4e7c612f70aeccf08220a3865935c66b70
johnny <joaomtomassilva@gmail.com>
2026-01-21 15:26:16 +0000

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
 import Metrics from '@/components/Metrics'
-import Logos from '@/components/Logos'
 import Comparison from '@/components/Comparison'
 import Solutions from '@/components/Solutions'
 import Benefits from '@/components/Benefits'
 import Process from '@/components/Process'
-import Testimonials from '@/components/Testimonials'
 import CTAForm from '@/components/CTAForm'
 import FAQ from '@/components/FAQ'
 import Footer from '@/components/Footer'
@@ -17,12 +15,10 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Metrics />
-      <Logos />
       <Comparison />
       <Solutions />
       <Benefits />
       <Process />
-      <Testimonials />
       <CTAForm />
       <FAQ />
       <Footer />
