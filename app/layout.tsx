2cc8ba146f987980fe19cc13bfadc537d3fed316
johnny <joaomtomassilva@gmail.com>
2026-01-21 17:25:12 +0000

    form submission fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-white text-slate-900 antialiased">
+        {/* Hidden form for Netlify form detection during build */}
+        <form
+          name="audit-request"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+          hidden
+        >
+          <input type="hidden" name="form-name" value="audit-request" />
+          <input type="hidden" name="bot-field" />
+          <input type="text" name="first_name" />
+          <input type="text" name="last_name" />
+          <input type="email" name="email" />
+          <input type="tel" name="phone" />
+          <input type="url" name="company_website" />
+        </form>
+        
         {children}
       </body>
     </html>
